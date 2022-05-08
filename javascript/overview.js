7e706c60f53a8627df4efe6db092b8c7d7a7cc28
AbdallahMehiz <abdallahmehiz0@gmail.com>
2022-05-08 22:52:19 +0100

    not much, it doesnt even work properly but

---
+++ javascript/overview.js
@@ -0,0 +1,10 @@
+
+let height = 1080;
+let width = 2340;
+
+if(width/height<1){
+    let overview = document.querySelector(".overview");
+    //overview.style.gridTemplateColumns = "1";
+    overview.style.log
+    console.log(overview.style);
+}
