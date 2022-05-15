279b3a556ef4676e8b43c270afdb8a3052416dec
AbdallahMehiz <abdallahmehiz0@gmail.com>
2022-05-15 16:53:07 +0100

    add padding when on phone

--- javascript/overview.js
+++ javascript/overview.js
@@ -4,4 +4,6 @@ console.log(window.innerWidth + " " + window.innerHeight)
 if(window.innerWidth/window.innerHeight < 1){
     let overview = document.querySelector(".overview")
     overview.style.display = "block"
+    let nav = document.querySelector(".currentsite")
+    nav.style.paddingLeft = "69px"
 }
